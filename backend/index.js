1e557c6855e8d8c71e4373d6742e7df0d6fc8ae4
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-21 15:22:56 -0500

    espacios de trabajo con rutas y modelos

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,22 @@ const cors = require("cors");
 const { dbConnection } = require("./bd/db");
 require("dotenv").config();
 
-const User = require("./routes/user");
-const Task = require("./routes/tasks");
-const Board = require("./routes/board")
 const Role = require('./routes/role');
-const Auth = require("./routes/auth")
+const Auth = require("./routes/auth");
+const User = require("./routes/user");
+const WorkSpace = require("./routes/workspace");
+const Task = require("./routes/tasks");
+const Board = require("./routes/board");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/role/", Role);
 app.use("/api/auth/", Auth)
 app.use("/api/user/", User);
+app.use("/api/workSpace/", WorkSpace);
 app.use("/api/board/",Task);
 app.use('/api/board/', Board);
-app.use("/api/role/", Role);
 
 app.listen(process.env.PORT, () =>
   console.log("Backend server running on port: " + process.env.PORT)
