5523e461db4786f3f3dbb9a4a44e19c3fb811817
Jalexss <jhalx2198@gmail.com>
2021-06-21 13:46:16 -0500

    Commented model workspaces

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Board = require("../models/board")
 const Auth = require("../middleware/auth")
 const User = require("../models/user")
-const Workspace = require("../models/workspace")
+// const Workspace = require("../models/workspace")
 
 router.post("/create",Auth, async (req, res) => {
   if (!req.body.name || !req.body.description || !req.body.techleader || !req.body.workspace) {
@@ -11,10 +11,10 @@ router.post("/create",Auth, async (req, res) => {
   }
   const user = await User.findOne({ name: req.body.techleader})
   if(!user) return res.status(400).send("User not found");
-  const workspace = await Workspace.findById(req.body.workspace)
-  if(!workspace) return res.status(400).send("Workspace not found");
+  // const workspace = await Workspace.findById(req.body.workspace)
+  // if(!workspace) return res.status(400).send("Workspace not found");
   const board = new Board({
-    workspace: workspace._id,
+    // workspace: workspace._id,
     name: req.body.name,
     description: req.body.description,
     tasks:[],
