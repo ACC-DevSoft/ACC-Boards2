8a93c401529cbd7d461d60c16f1a9cecc3aa07bc
Villadiego Hoyos <d.villadiego.hoyos@accenture.com>
2021-06-22 10:50:27 -0500

    feat: delete board and update board added

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -3,16 +3,13 @@ const router = express.Router();
 const Board = require("../models/board")
 const Auth = require("../middleware/auth")
 const User = require("../models/user")
-// const Workspace = require("../models/workspace")
 
-router.post("/create", async (req, res) => {
+router.post("/create",Auth, async (req, res) => {
   if (!req.body.name || !req.body.description || !req.body.techleader || !req.body.workspace) {
     return res.status(400).send("Incomplete Data");
   }
   const user = await User.findOne({ name: req.body.techleader})
   if(!user) return res.status(400).send("User not found");
-  // const workspace = await Workspace.findById(req.body.workspace)
-  // if(!workspace) return res.status(400).send("Workspace not found");
   const board = new Board({
     workspace: req.body.workspace,
     name: req.body.name,
@@ -30,11 +27,42 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.put('/list', async (req, res) => {
-  if(!req.body.workspace) return res.status(400).send("Incomplete Data");
-  const board = await Board.find({workspace: req.body.workspace})
-  if(!board) return res.status(400).send('workspace invalid')
+router.get('/list/:workspace', async (req, res) => {
+  if(!req.params.workspace) return res.status(400).send("Incomplete Data");
+  const board = await Board.find({workspace: req.params.workspace})
+  if(!board || board==[]) return res.status(400).send(' Not found boards on ' + req.params.workspace)
   res.status(200).send(board)
+})
+
+router.put('/update', async (req, res) => {
+  if (!req.body.board) return res.status(400).send('Incomplete Data')
+  let findBoard = await Board.findById(req.body.board)
+  if(!findBoard) return res.status(400).send('Board no exist')
+  if(req.body.name) findBoard.name = req.body.name
+  if(req.body.description) findBoard.description = req.body.description
+  if(req.body.techleader) {
+    const user = await User.findOne({ name: req.body.techleader})
+    if(!user) return res.status(400).send("User not found");
+    findBoard.techleader = user._id
+  }
+  if(req.body.status) findBoard.status = req.body.status
+  const board = await Board.findByIdAndUpdate(findBoard._id, {
+    name:findBoard.name,
+    description:findBoard.description,
+    techleader:findBoard.techleader,
+    status:findBoard.status,
+  })
+  if(!board) return res.status(400).send('Board no update') 
+  res.status(200).send('Board Update: ' + board)
+})
+
+router.delete('/delete/:board', async (req, res) => {
+  if (!req.params.board) return res.status(400).send('Incomplete Data')
+  let findBoard = await Board.findById(req.params.board)
+  if(!findBoard) return res.status(400).send('Board no exist')
+  const board = await Board.findByIdAndDelete(req.params.board)
+  if(!board) return res.status(400).send('Board no delete')
+  res.status(200).send('Board deleted ' + board)
 })
 
 module.exports = router
