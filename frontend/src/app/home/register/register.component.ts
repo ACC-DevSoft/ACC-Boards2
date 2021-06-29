fbe1cfa4a59e71b2d006a0f33f067cb317dcb8bd
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-06-29 12:03:37 -0500

    updated on 29-6-21 12:00

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public registerData: any;
+  public successMessage: String;
+  public errorMessage: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private auth: AuthService,private router: Router) { 
+    this.registerData = {};
+    this.successMessage = '';
+    this.errorMessage = '';
   }
 
+  ngOnInit(): void {}
+
+  registerUser() {
+    if (
+      !this.registerData.name ||
+      !this.registerData.email ||
+      !this.registerData.password ||
+      !this.registerData.userName
+    ){
+      console.log('Register Failed: Incomplete Data');
+      this.errorMessage = 'Register Failed: Incomplete Data';
+      this.closeAlert()
+      this.registerData = {};
+    } else {
+      this.auth.registerUser(this.registerData).subscribe(
+        (res: any) => {
+          console.log(res)
+          localStorage.setItem('token', res.jwtToken)
+          this.registerData = {};
+          this.router.navigate(['/'])
+        },
+        (err: any) => {
+          console.log(err)
+          this.errorMessage = err.error; 
+          this.closeAlert()
+          this.registerData = {};
+        }
+      );
+    }
+  }
+
+  closeAlert(){
+    setTimeout(() => {
+      this.successMessage = '';
+      this.errorMessage = '';
+    },3000);
+  }
+
+  closeX(){
+    this.successMessage = '';
+    this.errorMessage = '';
+  }
 }
