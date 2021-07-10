d6287e27cd58ba164f8177d0076b9b407a751240
Davidhzk2 <dherrera035@misena.edu.co>
2021-07-09 22:48:46 -0500

    backend

--- frontend/src/app/pipes/get-img.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetImgPipe } from './get-img.pipe';
-
-describe('GetImgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetImgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
