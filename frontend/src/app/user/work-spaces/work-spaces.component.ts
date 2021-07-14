f806643f65b74cc705fc08b47b860bc59fdfa402
Davidhzk2 <dherrera035@misena.edu.co>
2021-07-13 19:08:35 -0500

    ajustes a workspaces en diseño, funcionalidad de agregar wk

--- frontend/src/app/user/work-spaces/work-spaces.component.ts
+++ frontend/src/app/user/work-spaces/work-spaces.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig } from "@angular/material/dialog";
 import { AddWorkSpacesComponent } from './add-work-spaces/add-work-spaces.component';
+import { Router , ActivatedRoute} from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-work-spaces',
@@ -8,19 +10,30 @@ import { AddWorkSpacesComponent } from './add-work-spaces/add-work-spaces.compon
   styleUrls: ['./work-spaces.component.css']
 })
 export class WorkSpacesComponent implements OnInit {
+  public userid: any;
+  public userId:any;
   
   constructor(
+    private router:Router, private activated: ActivatedRoute,
+    private auth: AuthService,
     public dialog:MatDialog,
     //public dialogRef: MatDialogRef<AddWorkSpacesComponent>
-  ){ }
+  ){ 
+    this.userid = this.activated.snapshot.params.id;
+     this.userId = localStorage.getItem('current');
+
+  }
 
   ngOnInit(): void {
+    console.log(this.userid);
+    
   }
   openForm(): void{
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.width = '500px';
+    dialogConfig.data = this.userId;
     const dialogRef= this.dialog.open(AddWorkSpacesComponent, dialogConfig);
   }
   
